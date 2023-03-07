d89f12007f6722ec5eff16f028e2d7bdf2fd6c36
Alanthomas-2209 <2209@tkmce.ac.in>
2023-03-07 18:25:01 +0530

    some rearrangements

---
+++ other/Test/test.java
@@ -0,0 +1,18 @@
+package java_lab;
+import java.util.*;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world...!");
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the first data");
+		int a = scan.nextInt();
+		System.out.println("Enter the second data");
+		int b = scan.nextInt();
+		System.out.println(a+b);
+	}
+
+}

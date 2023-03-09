c0922aa5fa5a6e5d5ddeb98e811b3d23b81ee2c0
Alanthomas-2209 <2209@tkmce.ac.in>
2023-03-09 16:47:14 +0530

    some fixes

--- java_lab/CO1/q5.java
+++ java_lab/CO1/q5.java
@@ -6,8 +6,6 @@ import java_lab.CO1.cpu.Processor;
 class cpu {
 	
 	int price ;
-//	static RAM ram = new RAM();
-	
 	Scanner scan = new Scanner(System.in);
 	
 	class Processor {
@@ -21,42 +19,30 @@ class cpu {
 			cpu cpuobj = new cpu();
 			Processor processor = cpuobj.new Processor();
 			
-			void info() {
+			void insert() {
+				System.out.println("Enter the  No of the core and manufacturer of the Processor");
+				processor.no_cores = cpuobj.scan.nextInt();
+				processor.manufacturer = cpuobj.scan.next();
+				System.out.println("Enter the  size and manufacturer of the RAM");
+				memory = cpuobj.scan.nextInt();
+				manufacturer = cpuobj.scan.next();
+				System.out.println("Enter the price of the CPU");
+				cpuobj.price = cpuobj.scan.nextInt();
+			}
+			void display() {
 				System.out.println("Processor Manufacturer :" + processor.manufacturer +" Number of  cores :" + processor.no_cores);
 				System.out.println("RAM Manufacturer :" + this.manufacturer +" RAM size :" + this.memory);
 				System.out.println("Price of the CPU: " + cpuobj.price);
 			}
 		}
 	}
-	
-	 
-	
-	void insert() {
-		cpu cpuobj = new cpu();
-		Processor processor = cpuobj.new Processor();
-		Processor.RAM ram = new Processor.RAM();
-		
-		System.out.println("Enter the  No of the core and manufacturer of the Processor");
-		processor.no_cores = scan.nextInt();
-		processor.manufacturer = scan.next();
-		System.out.println("Enter the  size and manufacturer of the RAM");
-		ram.memory = scan.nextInt();
-		ram.manufacturer = scan.next();
-		System.out.println("Enter the price of the CPU");
-		this.price = scan.nextInt();
-	}
-	void info1() {
-		Processor.RAM product = new Processor.RAM();
-		product.info();
-	}
 }
 public class q5 {
 
 	public static void main(String[] args) {
-		cpu obj = new cpu();
-		obj.insert();
-		obj.info1();
-		
+			cpu.Processor.RAM object = new cpu.Processor.RAM();
+			object.insert();
+			object.display();
 	}
 
 }

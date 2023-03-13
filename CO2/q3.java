d05fd420b2356158dd72ebae5223bee77db471c5
Alanthomas-2209 <2209@tkmce.ac.in>
2023-03-13 11:42:43 +0530

    CO2 completed

--- CO2/q3.java
+++ CO2/q3.java
@@ -12,7 +12,7 @@ class functions {
 		System.out.println("converts the string to UPPERCASE:" + s1.toUpperCase());
 	}
 
-	void concat() {
+	void concatenate() {
 		String s1,s2;
 		System.out.println("Enter the First string:");
 		s1 = scan.next();
@@ -29,6 +29,26 @@ class functions {
 		int end =  scan.nextInt();
 		System.out.println("Substring: " + s1.substring(start,end));
 	}
+
+	void split () {
+		System.out.println("Enter the String :");
+		String str = scan.nextLine();
+		String[] array = str.split(" ");
+		System.out.println("String array:");
+		for(int i = 0 ;i < array.length ; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+	void replace () {
+		System.out.println("Enter the String:");
+		String str = scan.nextLine();
+		System.out.println("Enter the String you want to replace:");
+		String target = scan.nextLine();
+		System.out.println("Enter the String you want to replace with :");
+		String replacement = scan.nextLine();
+		System.out.println("Replaced String :" + str.replace(target,replacement));
+	}
 }
 
 public class q3 {
@@ -38,7 +58,7 @@ public class q3 {
 		functions obj = new functions();
 		Scanner scan = new Scanner(System.in);
 
-		System.out.println("OPTIONS:\n1.Basic Operations\n2.concatenate\n3.Substring\n0.For Exit");
+		System.out.println("OPTIONS:\n1.Basic Operations\n2.concatenate\n3.Substring\n4.String To Array\n5.Replace Character\n0.For Exit");
 		do {
 			System.out.println("Enter the choice:");
 			choice = scan.nextInt();
@@ -48,11 +68,19 @@ public class q3 {
 					break;
 				}
 				case 2:{
-					obj.concat();
+					obj.concatenate();
 					break;
 				}
 				case 3:{
 					obj.crop();
+					break;
+				}
+				case 4:{
+					obj.split();
+					break;
+				}
+				case 5:{
+					obj.replace();
 					break;
 				}
 				case 0:{
@@ -63,7 +91,6 @@ public class q3 {
 					System.out.println("Invalid Choice");
 					break;
 				}
-
 			}
 		}while(choice != 0);
 		scan.close();
--- CO3/q1.java
+++ CO3/q1.java
@@ -1,2 +1,7 @@
+import java.util.*;
+
 public class q1 {
+    public static void main(String[] args) {
+
+    }
 }

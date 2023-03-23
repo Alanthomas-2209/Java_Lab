2885df78cb21ff46e56cdc50155c572a2d355828
Alanthomas-2209 <2209@tkmce.ac.in>
2023-03-23 17:48:22 +0530

    co3 completed

--- CO3/q5.java
+++ CO3/q5.java
@@ -30,11 +30,42 @@ class Result extends  Sports {
         this.Rank = rank;
     }
     void display() {
-        System.out.println("Roll NO: " + rollNo + " Name: " + Name + " Department: " + Department +" Mark: " + academicMark + " Grade: " + sportsGrade +" Rank: " + this.Rank);
+        System.out.println("Roll NO: " + rollNo + " Name: " + Name + " Department: " + Department +" Academic Mark: " + academicMark + " Grade: " + sportsGrade +" Rank: " + this.Rank);
+    }
+}
+
+class methods {
+    Scanner scan = new Scanner(System.in);
+    Result[] object;
+    void insert() {
+        System.out.println("Enter the Number of student:");
+        int n = scan.nextInt();
+        object = new Result[n];
+        for(int i = 0; i < object.length; i++){
+            System.out.println("Enter the Roll No, Name, Department and Academic Mark:");
+            int rollno = scan.nextInt();
+            String name = scan.next();
+            String department = scan.next();
+            int aMark = scan.nextInt();
+            System.out.println("Enter the Grade in Sports:");
+            String grade = scan.next();
+            System.out.println("Enter the Rank:");
+            int rank = scan.nextInt();
+            object[i] = new Result(rollno, name,  department, aMark, grade, rank);
+        }
+    }
+    void show() {
+        System.out.println("Displaying");
+        for(int i = 0; i < object.length; i++){
+            object[i].display();
+            System.out.println("---------------------------------------------------------------------");
+        }
     }
 }
 public class q5 {
     public static void main(String[] args) {
-        
+        methods obj = new methods();
+        obj.insert();
+        obj.show();
     }
 }

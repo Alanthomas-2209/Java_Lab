8f5929da21af29266d82c4cfe1dc99736f3d5fc0
Alanthomas-2209 <2209@tkmce.ac.in>
2023-04-03 12:11:57 +0530

    minor addition

--- CO4/Question4.java
+++ CO4/Question4.java
@@ -4,9 +4,10 @@ public class Question4 {
     Scanner scan = new Scanner(System.in);
     public static void main(String[] Args) {
         int number = -1;
-
-        try(number < 0){
-            System.out.println("User can't input negative values....!");
+        try {
+            system.out.println("User can't input negative values....!");
+        }
+        catch (number < 0){
         }
     }
 }

11cc4c4fe17f0f292cddc2ff21e412ab210fe52d
Alanthomas-2209 <2209@tkmce.ac.in>
2023-06-22 16:08:24 +0530

    applet updated

--- CO5/Question1.java
+++ CO5/Question1.java
@@ -2,7 +2,7 @@ package java_lab.CO5;
 import java.awt.*;
 import java.applet.*;
 import java.util.*;
-public class Question1 extends  Applet{
+public class Question1 extends Applet{
     public void paint(Graphics g){
         g.setColor(Color.blue);
         g.drawLine(10,10,100,100);

28b79bf95b161418eec0d96503bff9e6daa02ae5
Alanthomas-2209 <2209@tkmce.ac.in>
2023-07-14 15:30:52 +0530

    Dequeue

--- CO4/Question14.java
+++ CO4/Question14.java
@@ -2,29 +2,56 @@ package java_lab.CO4;
 import java.util.*;
 
 public class Question14 {
-    static Deque<Integer> queue = new ArrayDeque<>();
-    public static void main(String[] Args){
-        Scanner scan = new Scanner(System.in);
 
-        System.out.println("How many number want to insert:");
-        int n = scan.nextInt();
-        System.out.println("Enter the numbers to Enqueue:");
-        for (int i = 0; i < n; i++) {
-            int value = scan.nextInt();
-            queue.add(value);
+    public static void main(String[] Args){
+        Deque<Integer> queue = new ArrayDeque<>();
+        Scanner scan = new Scanner(System.in);
+        int choice = 1;
+
+        System.out.println("OPTIONS:\n1.Insertion at Rear\n2.Insertion at Front\n3.Display\n4.Remove from Front\n5.Removed from last\n0.Exit");
+        while (choice != 0) {
+            System.out.println("\nEnter the choice:");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("How many number want to insert:");
+                    int n = scan.nextInt();
+                    System.out.println("Enter the numbers to Enqueue:");
+                    for (int i = 0; i < n; i++) {
+                        int value = scan.nextInt();
+                        queue.offerLast(value);
+                    }
+                }
+                case 2 -> {
+                    System.out.println("How many number want to insert:");
+                    int n = scan.nextInt();
+                    System.out.println("Enter the numbers to Enqueue:");
+                    for (int i = 0; i < n; i++) {
+                        int value = scan.nextInt();
+                        queue.offerFirst(value);
+                    }
+                }
+                case 3 -> {
+                    System.out.println("Iterate through queue using Iterator:");
+                    Iterator iterator = queue.iterator();
+                    while (iterator.hasNext())
+                        System.out.print(iterator.next() + "->");
+                }
+                case 4 -> {
+                    System.out.println("The element removed from the Front: " + queue.pollFirst());
+                    System.out.println("Queue After operation: " + queue);
+                }
+                case 5 -> {
+                    System.out.println("The element removed from the last: " + queue.pollLast());
+                    System.out.println("Queue After operation: " + queue);
+                }
+                case 0 -> {
+                    System.out.println("Exiting.....");
+                }
+                default -> {
+                    System.out.println("Invalid Operation....! Please try again...");
+                }
+            }
         }
-        display();
-        System.out.println("\nHow many number want to Deque:");
-        int nToDelete = scan.nextInt();
-        for (int i = 0; i < nToDelete; i++) {
-            queue.pop();
-        }
-        display();
-    }
-    public static void display() {
-        System.out.println("Iterate through queue using Iterator:");
-        Iterator iterator = queue.iterator();
-        while(iterator.hasNext())
-            System.out.print(iterator.next() + "->");
     }
 }
